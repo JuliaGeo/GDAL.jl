d76caa2263fbd64560bb1057a11698e3b925b9ee
Martijn Visser <mgvisser@gmail.com>
2016-03-11 15:24:19 +0700

    Fix test, less stringent projection WKT check
    The last digits of `SPHEROID["Clarke 1866",6378206.4,294.9786982139006` seem to be different between GDAL 2.0 and trunk.

--- test/tutorial_raster.jl
+++ test/tutorial_raster.jl
@@ -13,7 +13,8 @@ driver = GDAL.gdalgetdriverbyname("GTiff")
 @test GDAL.getrasterxsize(dataset) == 100
 @test GDAL.getrasterysize(dataset) == 100
 @test GDAL.getrastercount(dataset) == 1
-@test GDAL.getprojectionref(dataset) == """PROJCS["NAD27 / UTM zone 11N",GEOGCS["NAD27",DATUM["North_American_Datum_1927",SPHEROID["Clarke 1866",6378206.4,294.9786982138982,AUTHORITY["EPSG","7008"]],AUTHORITY["EPSG","6267"]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433],AUTHORITY["EPSG","4267"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-117],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AUTHORITY["EPSG","26711"]]"""
+nad27_prefix = "PROJCS[\"NAD27 / UTM zone 11N\",GEOGCS[\"NAD27\",DATUM[\"North_American_Datum_1927\","
+@test startswith(GDAL.getprojectionref(dataset), nad27_prefix)
 
 geotransform = zeros(6)
 GDAL.getgeotransform(dataset, geotransform)
