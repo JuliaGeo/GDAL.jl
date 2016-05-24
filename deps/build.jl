483099e5613579d51ca2ae657d3c95e0a9876043
Martijn Visser <mgvisser@gmail.com>
2016-05-25 01:01:10 +0700

    build.jl try validation function to keep out GDAL 1.x

--- deps/build.jl
+++ deps/build.jl
@@ -3,8 +3,18 @@ using Conda
 
 @BinDeps.setup
 
-libgdal = library_dependency("libgdal", aliases=["gdal","gdal111","gdal200","gdal201",
-                                                 "gdal_w32","gdal_w64"])
+function isgdal2(name, handle)
+    fptr = Libdl.dlsym(handle, :GDALVersionInfo)
+    versionptr = ccall(fptr,Cstring,(Cstring,),"RELEASE_NAME")
+    versionstring = bytestring(versionptr)
+    gdalversion = convert(VersionNumber, versionstring)
+    gdalversion >= v"2.0.0"
+end
+
+libgdal = library_dependency("libgdal",
+                             aliases=["gdal","gdal210","gdal201","gdal200",
+                                      "gdal_w32","gdal_w64"],
+                             validate=isgdal2)
 
 @windows_only provides(Conda.Manager, "libgdal", libgdal)
 @unix_only provides(Conda.Manager, "libgdal", libgdal)
