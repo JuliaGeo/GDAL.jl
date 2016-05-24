0ec9047c9780462a9a7d6316a51ebafd81fb6aeb
Martijn Visser <mgvisser@gmail.com>
2016-05-25 00:02:25 +0700

    use default libgdal on windows, add gdal200 alias

--- deps/build.jl
+++ deps/build.jl
@@ -3,11 +3,10 @@ using Conda
 
 @BinDeps.setup
 
-libgdal = library_dependency("libgdal", aliases=["gdal","gdal111","gdal201",
+libgdal = library_dependency("libgdal", aliases=["gdal","gdal111","gdal200","gdal201",
                                                  "gdal_w32","gdal_w64"])
 
-# install older gdal on windows
-@windows_only provides(Conda.Manager, "libgdal==1.11.2", libgdal)
+@windows_only provides(Conda.Manager, "libgdal", libgdal)
 @unix_only provides(Conda.Manager, "libgdal", libgdal)
 
 @BinDeps.install Dict(:libgdal => :libgdal)
