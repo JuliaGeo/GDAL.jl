8f6007f11ead22c52048782381542348670aa6e3
Martijn Visser <mgvisser@gmail.com>
2016-06-29 22:27:24 +0200

    remove gdal111 alias
    On OSGEO4W stable it remains installed as such besides gdal201 (2.1.0)
    In my install it thus settled for gdal111, possibly because it is listed first.

--- deps/build.jl
+++ deps/build.jl
@@ -3,14 +3,14 @@ using Conda
 
 @BinDeps.setup
 
-libgdal = library_dependency("libgdal", aliases=["gdal","gdal111","gdal201",
+libgdal = library_dependency("libgdal", aliases=["gdal","gdal201",
                                                  "gdal_w32","gdal_w64","libgdal-20"])
 
 @windows_only begin
     using WinRPM
     push!(WinRPM.sources, "http://download.opensuse.org/repositories/home:yeesian/openSUSE_13.2")
     WinRPM.update()
-    provides(WinRPM.RPM, "libgdal", [libgdal], os = :Windows)
+    provides(WinRPM.RPM, "gdal", [libgdal], os = :Windows)
 end
 @unix_only provides(Conda.Manager, "libgdal", libgdal)
 
