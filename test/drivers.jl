18174e2b734f40a9c2c2c41bce17925e1be71ccc
Martijn Visser <mgvisser@gmail.com>
2019-08-22 21:22:42 +0200

    test_broken some drivers we want to add

--- test/drivers.jl
+++ test/drivers.jl
@@ -1,9 +1,11 @@
+# Test the availability of drivers.
+# See https://github.com/JuliaGeo/GDAL.jl/issues/65 for an overview.
+
 # not a complete list, but these drivers are tested for availability
 available_drivers = [
     # raster drivers
     "AAIGrid",
     "GPKG",
-    # "JP2OpenJPEG",  # up next, see #64
     "GTiff",
     "MEM",
     "PCRaster",
@@ -22,4 +24,33 @@ available_drivers = [
 
 for drivername in available_drivers
     @test GDAL.gdalgetdriverbyname(drivername) != C_NULL
+end
+
+not_available_drivers = [
+    # libcurl raster drivers
+    "EEDAI",
+    "PLMosaic",
+    "RDA",
+    "WCS",
+    "WMS",
+    "WMTS",
+    # libcurl vector drivers
+    "AmigoCloud",
+    "Carto",
+    "Cloudant",
+    "CouchDB",
+    "CSW",
+    "EEDA",
+    "ElasticSearch",
+    "GFT",
+    "NGW",
+    "PLScenes",
+    "WFS",
+    "WFS3",
+    "Carto",
+    "JP2OpenJPEG",  # up next, see #64
+]
+
+for drivername in not_available_drivers
+    @test_broken GDAL.gdalgetdriverbyname(drivername) != C_NULL
 end
