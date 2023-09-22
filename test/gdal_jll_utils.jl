c6ef9fd947c4b98b1ce600b35ef342ae20668d93
Martijn Visser <mgvisser@gmail.com>
2023-09-22 22:37:36 +0200

    stop testing deprecated form

--- test/gdal_jll_utils.jl
+++ test/gdal_jll_utils.jl
@@ -1,9 +1,4 @@
 # This is testing GDAL/OGR utility executables.
 # In `gdal_utils.jl` we test the utility library functions.
 
-@test success(GDAL.gdalinfo_path() do gdinfo
-    run(pipeline(`$gdinfo $utmsmall`, devnull))
-end)
-
-# preferred thread-safe wrapper syntax on Julia 1.6+
 @test success(run(pipeline(`$(GDAL.gdalinfo_path()) $utmsmall`, devnull)))
